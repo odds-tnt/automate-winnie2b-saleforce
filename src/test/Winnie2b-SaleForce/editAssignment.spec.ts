12d816049f8629378b7c2df15d0eb57fc8592d76
MAkra09 <97652809+MAkra09@users.noreply.github.com>
2025-02-24 16:07:16 +0700

    เพิ่มEdit File--แก้ไขชื่อไฟล์

--- src/test/Winnie2b-SaleForce/editAssignment.spec.ts
+++ src/test/Winnie2b-SaleForce/editAssignment.spec.ts
@@ -1,53 +1,49 @@
 import { test } from "@playwright/test";
 import { LoginPageSaleForce } from "../../page-object/LoginPage";
 import { testData_allAssignment } from "../../test-data/user-saleforce/user-saleforce-dev";
-import { dataTest_VisitForm } from "../../test-data/Data-AssignmentForm/Data-AssignmentForm";
-import { urlSaleForcecEnvDev, urlSaleForcecEnvUAT } from "../../test-data/url-saleforce/url-saleforce";
+import { dataTestAssignmentForm } from "../../test-data/Data-AssignmentForm/Data-AssignmentForm";
+import {
+  urlSaleForcecEnvDev,
+  urlSaleForcecEnvUAT,
+} from "../../test-data/url-saleforce/url-saleforce";
 import { EditAssignment } from "../../page-object/EditAssignment";
 import { OwnerEmail } from "../../test-data/Data-AssignmentForm/Edit-dataForm";
 
 test.beforeEach(async ({ page }) => {
-    const loginpagesaleforce = new LoginPageSaleForce(page);
-    await page.goto(urlSaleForcecEnvUAT.saleForceLogInPage_UAT);
-    await loginpagesaleforce.FillEmailForLoginWinnie2bSaleForce(
-      testData_allAssignment.email
-    );
-    await loginpagesaleforce.FillPassWordForLoginWinnie2bSaleForce(
-      testData_allAssignment.password
-    );
-    await loginpagesaleforce.ClickSubmitForLoginWinnie2bSaleForce();
-    await loginpagesaleforce.CheckLoginWinnie2bSaleForceSuccessForAllAssign();
-  });
+  const loginpagesaleforce = new LoginPageSaleForce(page);
+  await page.goto(urlSaleForcecEnvUAT.saleForceLogInPage_UAT);
+  await loginpagesaleforce.loginWinnie2bSaleForce(
+    testData_allAssignment.email,
+    testData_allAssignment.password
+  );
+});
 
 test.afterEach(async ({ page }) => {
-    await page.waitForTimeout(5000);
-    await page.close();
+  await page.waitForTimeout(5000);
+  await page.close();
+});
+
+test.describe("ทดสอบระบบ Winnie2b SaleForce", async () => {
+  test("ทดสอบการ Edit Assignment", async ({ page }) => {
+    const editAssignment = new EditAssignment(page);
+    await editAssignment.ClickViewAssignmentShowAll();
+    await editAssignment.SearchAssignment(dataTestAssignmentForm.StoreName);
+    await page.waitForTimeout(3000);
+    await editAssignment.SelectAssignment();
+    await editAssignment.ClickEditAssignment();
+    await editAssignment.EditVisitDate();
+    await editAssignment.EditOwnerEmail(OwnerEmail.Email);
+    await editAssignment.ClickSaveEdit();
+    await editAssignment.CheckEditVisitAssignmentSuccess();
   });
 
-  test.describe("ทดสอบระบบ Winnie2b SaleForce", async () => {
-    test("ทดสอบการ Edit Assignment", async ({ page }) => {
-    const editAssignment = new EditAssignment(page)
-    await editAssignment.ClickViewAssignmentShowAll()
-    await editAssignment.SearchAssignment(dataTest_VisitForm.StoreName)
-    await page.waitForTimeout(3000)
-    await editAssignment.SelectAssignment()
-    await editAssignment.ClickEditAssignment()
-    await editAssignment.EditVisitDate()
-    await editAssignment.EditOwnerEmail(OwnerEmail.Email)
-    await editAssignment.ClickSaveEdit()
-    await editAssignment.CheckEditVisitAssignmentSuccess()
-      
-    })
-
-    test("ทดสอบการ Edit Email ที่ไม่มีในระบบ", async ({ page }) => {
-      const editAssignment = new EditAssignment(page)
-      await editAssignment.ClickViewAssignmentShowAll()
-      await editAssignment.SearchAssignment(dataTest_VisitForm.StoreName)
-      await page.waitForTimeout(3000)
-      await editAssignment.SelectAssignment()
-      await editAssignment.ClickEditAssignment()
-      await editAssignment.EditErrorOwnerEmail(OwnerEmail.Email_Error)
-
-        
-      })
-  })
+  test("ทดสอบการ Edit Email ที่ไม่มีในระบบ", async ({ page }) => {
+    const editAssignment = new EditAssignment(page);
+    await editAssignment.ClickViewAssignmentShowAll();
+    await editAssignment.SearchAssignment(dataTestAssignmentForm.StoreName);
+    await page.waitForTimeout(3000);
+    await editAssignment.SelectAssignment();
+    await editAssignment.ClickEditAssignment();
+    await editAssignment.EditErrorOwnerEmail(OwnerEmail.Email_Error);
+  });
+});
