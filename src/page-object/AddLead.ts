45db5dd1221cd43b0776a8e17322f9eb2e4fb4e1
Yanisa <yanisa.p@odds.team>
2025-04-04 14:17:58 +0700

    add lead visit choice

--- src/page-object/AddLead.ts
+++ src/page-object/AddLead.ts
@@ -9,10 +9,14 @@ export class AddLead {
   readonly displayLeadForm: Locator;
   readonly registeredBtn: Locator;
   readonly customerIdInput: Locator;
-  readonly visitChoiceForRegitered: Locator;
   readonly orderedBtn: Locator;
   readonly orderIdInput: Locator;
   readonly inconvenientBtn: Locator;
+  readonly notRegisteredBtn: Locator;
+  readonly waitingForDecisionBtn: Locator;
+  readonly notFoundOwnerBtn: Locator;
+  readonly notConvenientToTalkBtn: Locator;
+  readonly notInterestedBtn: Locator;
   readonly visitSummary: Locator;
   readonly submitBtn: Locator;
 
@@ -21,27 +25,33 @@ export class AddLead {
     this.storeNameInput = page.getByTestId("StoreName");
     this.customerNameInput = page.getByTestId("CustomerName");
     this.addressInput = page.getByTestId("address");
-    this.nextBtn = page.getByTestId("next-button"); 
-    this.displayLeadForm = page.getByTestId("visit-button-A13")
-    this.registeredBtn = page.getByTestId("visit-button-A13")
+    this.nextBtn = page.getByTestId("next-button");
+    this.displayLeadForm = page.getByTestId("visit-button-A13");
+    this.registeredBtn = page.getByTestId("visit-button-A13");
     this.customerIdInput = page.getByTestId("orderId"); //ต้องแก้ชื่อ datatestid
-    this.visitChoiceForRegitered = page.getByTestId("visit-button-B17");
     this.orderedBtn = page.getByTestId("visit-button-B17");
     this.orderIdInput = page.getByTestId("orderId");
     this.inconvenientBtn = page.getByTestId("visit-button-B18");
+    this.notRegisteredBtn = page.getByTestId("visit-button-A14");
+    this.waitingForDecisionBtn = page.getByTestId("visit-button-B19");
+    this.notFoundOwnerBtn = page.getByTestId("visit-button-B20");
+    this.notConvenientToTalkBtn = page.getByTestId("visit-button-B21");
+    this.notInterestedBtn = page.getByTestId("visit-button-B22");
     this.visitSummary = page.getByText("ผลการเข้าเยี่ยม");
     this.submitBtn = page.getByTestId("submit-button");
   }
 
-  async fillLeadInfo(){
+  async fillLeadInfo() {
     await this.storeNameInput.click();
     await this.storeNameInput.fill("ร้านป้านง");
     await this.customerNameInput.click();
     await this.customerNameInput.fill("นงนุช");
     await this.addressInput.click();
-    await this.addressInput.fill("101 ถนนพระราม 6 แขวงพญาไท เขตพญาไท กรุงเทพมหานคร 10400");
+    await this.addressInput.fill(
+      "101 ถนนพระราม 6 แขวงพญาไท เขตพญาไท กรุงเทพมหานคร 10400"
+    );
     await this.nextBtn.click();
-    expect(this.displayLeadForm).toBeVisible();
+    // expect(this.displayLeadForm).toBeVisible();
   }
 
   async selectRegistered() {
@@ -49,7 +59,6 @@ export class AddLead {
     await this.customerIdInput.click();
     await this.customerIdInput.fill("5454");
     await this.nextBtn.click();
-    expect(this.visitChoiceForRegitered).toBeVisible();
   }
 
   async selectOrdered() {
@@ -62,6 +71,35 @@ export class AddLead {
 
   async selectInconvenient() {
     await this.inconvenientBtn.click();
+    await this.nextBtn.click();
+    expect(this.visitSummary).toBeVisible();
+  }
+
+  async selectNotRegistered() {
+    await this.notRegisteredBtn.click();
+    await this.nextBtn.click();
+  }
+
+  async selectWaittingForDecision() {
+    await this.waitingForDecisionBtn.click();
+    await this.nextBtn.click();
+    expect(this.visitSummary).toBeVisible();
+  }
+
+  async selectNotFoundOwnerBtn() {
+    await this.notFoundOwnerBtn.click();
+    await this.nextBtn.click();
+    expect(this.visitSummary).toBeVisible();
+  }
+
+  async selectNotConvienientToTalk() {
+    await this.notConvenientToTalkBtn.click();
+    await this.nextBtn.click();
+    expect(this.visitSummary).toBeVisible();
+  }
+
+  async selectNotInterested() {
+    await this.notInterestedBtn.click();
     await this.nextBtn.click();
     expect(this.visitSummary).toBeVisible();
   }
--- src/test/Winnie2b-SaleForce/addVisitLead.spec.ts
+++ src/test/Winnie2b-SaleForce/addVisitLead.spec.ts
@@ -15,7 +15,7 @@ test.afterEach(async ({ page }) => {
 });
 
 test.describe("เข้าเยี่ยมร้านค้าใหม่", async () => {
-  test("เข้าเยี่ยมร้านค้าใหม่: ลงทะเบียนแล้ว)", async ({
+  test("เข้าเยี่ยมร้านค้าใหม่: ลงทะเบียนและสั่งซื้อสินค้าแล้ว)", async ({
     page,
   }) => {
     const loginpagesaleforce = new LoginPageSaleForce(page);
@@ -34,7 +34,7 @@ test.describe("เข้าเยี่ยมร้านค้าใหม่",
     await addLead.clickSubmitButton();
   });
 
-  test("เข้าเยี่ยมร้านค้าใหม่: ไม่สะดวกทำการสั่งซื้อในช่วงเวลาเข้าเยี่ยม )", async ({
+  test("เข้าเยี่ยมร้านค้าใหม่: ลงทะเบียน แต่ไม่สะดวกทำการสั่งซื้อในช่วงเวลาเข้าเยี่ยม )", async ({
     page,
   }) => {
     const loginpagesaleforce = new LoginPageSaleForce(page);
@@ -52,5 +52,78 @@ test.describe("เข้าเยี่ยมร้านค้าใหม่",
     await addLead.selectInconvenient();
     await addLead.clickSubmitButton();
   });
-  
-})
+
+  test("เข้าเยี่ยมร้านค้าใหม่: ยังไม่ลงทะเบียนและรอตัดสินใจ )", async ({
+    page,
+  }) => {
+    const loginpagesaleforce = new LoginPageSaleForce(page);
+    const displayLead = new LeadList(page);
+    const addLead = new AddLead(page);
+    await loginpagesaleforce.loginWinnie2bSaleForce(
+      testData_allAssignment.email,
+      testData_allAssignment.password
+    );
+    await loginpagesaleforce.checkLoginSuccessForAllAssignmentRole();
+    await displayLead.leadListPage();
+    await displayLead.leadInfoPage();
+    await addLead.fillLeadInfo();
+    await addLead.selectNotRegistered();
+    await addLead.selectWaittingForDecision();
+    await addLead.clickSubmitButton();
+  });
+
+  test("เข้าเยี่ยมร้านค้าใหม่: ยังไม่ลงทะเบียน เจ้าของร้านไม่อยู่ )", async ({
+    page,
+  }) => {
+    const loginpagesaleforce = new LoginPageSaleForce(page);
+    const displayLead = new LeadList(page);
+    const addLead = new AddLead(page);
+    await loginpagesaleforce.loginWinnie2bSaleForce(
+      testData_allAssignment.email,
+      testData_allAssignment.password
+    );
+    await loginpagesaleforce.checkLoginSuccessForAllAssignmentRole();
+    await displayLead.leadListPage();
+    await displayLead.leadInfoPage();
+    await addLead.fillLeadInfo();
+    await addLead.selectNotRegistered();
+    await addLead.selectNotFoundOwnerBtn();
+    await addLead.clickSubmitButton();
+  });
+
+  test("เข้าเยี่ยมร้านค้าใหม่: ยังไม่ลงทะเบียน ไม่สะดวกคุย )", async ({
+    page,
+  }) => {
+    const loginpagesaleforce = new LoginPageSaleForce(page);
+    const displayLead = new LeadList(page);
+    const addLead = new AddLead(page);
+    await loginpagesaleforce.loginWinnie2bSaleForce(
+      testData_allAssignment.email,
+      testData_allAssignment.password
+    );
+    await loginpagesaleforce.checkLoginSuccessForAllAssignmentRole();
+    await displayLead.leadListPage();
+    await displayLead.leadInfoPage();
+    await addLead.fillLeadInfo();
+    await addLead.selectNotRegistered();
+    await addLead.selectNotConvienientToTalk();
+    await addLead.clickSubmitButton();
+  });
+
+  test("เข้าเยี่ยมร้านค้าใหม่: ยังไม่ลงทะเบียน ไม่สนใจ )", async ({ page }) => {
+    const loginpagesaleforce = new LoginPageSaleForce(page);
+    const displayLead = new LeadList(page);
+    const addLead = new AddLead(page);
+    await loginpagesaleforce.loginWinnie2bSaleForce(
+      testData_allAssignment.email,
+      testData_allAssignment.password
+    );
+    await loginpagesaleforce.checkLoginSuccessForAllAssignmentRole();
+    await displayLead.leadListPage();
+    await displayLead.leadInfoPage();
+    await addLead.fillLeadInfo();
+    await addLead.selectNotRegistered();
+    await addLead.selectNotInterested();
+    await addLead.clickSubmitButton();
+  });
+});
